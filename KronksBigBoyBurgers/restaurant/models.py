25cf6c21d61aba9504cb365b1d3258ad8ff09444
Dom123x456 <>
2023-04-10 18:52:57 +0000

    added class tablecatergory with choices being standard/deluxe

--- KronksBigBoyBurgers/restaurant/models.py
+++ KronksBigBoyBurgers/restaurant/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse_lazy
 # Create your models here.
+
+class TableCategory(models.Model):
+    title = models.CharField(max_length=50)
+
+    STANDARD = 'Standard'
+    DELUXE = 'Deluxe'
+
+    CATEGORY_CHOICES = [
+        (STANDARD, 'Standard'),
+        (DELUXE, 'Deluxe'),
+    ]
+
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
