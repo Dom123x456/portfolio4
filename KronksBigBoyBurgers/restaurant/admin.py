9dad78f4cd4e938145c4a51b4671cccc28d0dafc
Dom123x456 <>
2023-04-10 20:36:31 +0000

    deleted all extra imports except table category on line 2

--- KronksBigBoyBurgers/restaurant/admin.py
+++ KronksBigBoyBurgers/restaurant/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, Table, TableCategory, TableBooking
+from .models import TableCategory
 
 # Register your models here.
 admin.site.register(TableCategory)
