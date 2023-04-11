4229cd140783b093698e7a11082423755f92640f
Dom123x456 <>
2023-04-11 14:18:03 +0000

    added admin.site.register(RestaurantBooking) and to import

--- KronksBigBoyBurgers/restaurant/admin.py
+++ KronksBigBoyBurgers/restaurant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TableCategory
+from .models import TableCategory, RestaurantBooking
 
 # Register your models here.
 admin.site.register(TableCategory)
+admin.site.register(RestaurantBooking)
