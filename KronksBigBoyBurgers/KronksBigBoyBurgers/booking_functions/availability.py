3f4b274ba2ece7c8ad0a7c35708d3f0061c087b8
Dom123x456 <>
2023-04-11 15:45:17 +0000

    Checks table availability of restaurant reservation during a time window

--- KronksBigBoyBurgers/KronksBigBoyBurgers/booking_functions/availability.py
+++ KronksBigBoyBurgers/KronksBigBoyBurgers/booking_functions/availability.py
@@ -2,3 +2,13 @@ import datetime
 from restaurant.models import Table, RestaurantBooking
 
 def check_table_availability(table, reservation_start, reservation_end):
+    availability = []
+    reservations = RestaurantBooking.objects.filter(table=table)
+    
+    for reservation in reservations:
+        if reservation.reservation_start > reservation_end or reservation.reservation_end < reservation_start:
+            availability.append(True)
+        else:
+            availability.append(False)
+    
+    return all(availability)
