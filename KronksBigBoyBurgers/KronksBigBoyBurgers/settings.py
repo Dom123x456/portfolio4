dba559f5811da047f6717694187490ad5d203cc0
Dom123x456 <>
2023-04-11 15:26:23 +0000

    added #my apps to line 43 with restaurant.booking_functions

--- KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
+++ KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#My apps
     'restaurant',
+    'restaurant.booking_functions',
 ]
 
 MIDDLEWARE = [
