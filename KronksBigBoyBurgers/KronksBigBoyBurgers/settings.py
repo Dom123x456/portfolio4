655fc2b7ac960c03ecdefe6420ab10a3261dd3d2
Dom123x456 <>
2023-04-12 19:02:26 +0000

    changed build paths inside the project like using os

--- KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
+++ KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
@@ -19,9 +19,10 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env()
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
