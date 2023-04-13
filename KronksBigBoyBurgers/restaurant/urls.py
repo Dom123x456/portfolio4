18716c20ebc75609f3a12c3f5c2af4102a5b0838
Dom123x456 <>
2023-04-13 13:44:48 +0000

    added url patterns path #4 empty

--- KronksBigBoyBurgers/restaurant/urls.py
+++ KronksBigBoyBurgers/restaurant/urls.py
@@ -2,3 +2,10 @@ from django.urls import path
 from .views import (ReservationFormView, ReservationListView, TableDetailView, CancelReservationView,)
 
 app_name = "restaurant"
+
+urlpatterns = [
+    path(),
+    path(),
+    path(),
+    path(),
+]
