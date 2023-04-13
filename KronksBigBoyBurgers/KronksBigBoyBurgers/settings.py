a19c19ac4aee0342f56d9effb4832bc55a197893
Dom123x456 <>
2023-04-13 12:56:52 +0000

    Configures REST API permissions & password validation settings

--- KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
+++ KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
@@ -105,6 +105,14 @@ if DEBUG == True:
     }
 else:
     DATABASES = {'default': dj_database_url.config()}
+    
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Password validation
