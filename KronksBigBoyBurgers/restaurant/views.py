6620b1d620ad0b455de90818cbc911e64f88f9ea
Dom123x456 <>
2023-04-13 13:57:46 +0000

    Handle POST request to reserve a table and redirect to checkout view

--- KronksBigBoyBurgers/restaurant/views.py
+++ KronksBigBoyBurgers/restaurant/views.py
@@ -22,3 +22,15 @@ class ReservationFormView(View):
         else:
             form = AvailabilityForm()
         return render(request, 'reservation_form.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AvailabilityForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            request.session['reservation_start'] = data['reservation_start'].strftime("%Y-%m-%dT%H:%M")
+            request.session['reservation_end'] = data['reservation_end'].strftime("%Y-%m-%dT%H:%M")
+            request.session['table_category'] = data['table_category'].category
+            request.session['amount'] = find_total_table_charge(data['reservation_start'], data['reservation_end'], data['table_category'])
+            return redirect('restaurant:CheckoutView')
+        return HttpResponse('form not valid', form.errors)
