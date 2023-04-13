f3d58663153e3d4fff078417b68969823f991f6d
Dom123x456 <>
2023-04-13 13:48:37 +0000

    Loads environment variables from a .env file

--- KronksBigBoyBurgers/restaurant/views.py
+++ KronksBigBoyBurgers/restaurant/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListViews
 from .models import Table, RestaurantBooking
 # Create your views here.
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
