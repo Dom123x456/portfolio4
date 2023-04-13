329174dff920bd0ab4fb0d51dfc37e73beb2f61a
Dom123x456 <>
2023-04-13 12:45:17 +0000

    Sets SQLite DB for local dev, uses remote DB for production

--- KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
+++ KronksBigBoyBurgers/KronksBigBoyBurgers/settings.py
@@ -96,12 +96,15 @@ WSGI_APPLICATION = 'KronksBigBoyBurgers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
