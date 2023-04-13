b5a36d44d98e019c8f1590ca0550848ea094bdda
Dom123x456 <>
2023-04-13 13:54:42 +0000

    Add ReservationFormView for GET request of reservation form

--- KronksBigBoyBurgers/restaurant/views.py
+++ KronksBigBoyBurgers/restaurant/views.py
@@ -8,3 +8,17 @@ env = environ.Env(
 )
 
 environ.Env.read_env()
+
+class ReservationFormView(View):
+    def get(self, request, *args, **kwargs):
+        if "reservation_start" in request.session:
+            s = request.session
+            form_data = {
+                "reservation_start": s['reservation_start'],
+                "reservation_end": s['reservation_end'],
+                "table_category": s['table_category']
+            }
+            form = AvailabilityForm(request.POST or None, initial=form_data)
+        else:
+            form = AvailabilityForm()
+        return render(request, 'reservation_form.html', {'form': form})
