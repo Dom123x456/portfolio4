e9132e44dbafe95638dd004a7e979439ecf0173c
Dom123x456 <>
2023-04-13 15:33:33 +0000

    Handle reservation availability with AvailabilityForm validation

--- KronksBigBoyBurgers/restaurant/forms.py
+++ KronksBigBoyBurgers/restaurant/forms.py
@@ -1,2 +1,17 @@
 from django import forms
 from .models import Table
+
+class AvailabilityForm(forms.Form):
+    reservation_start = forms.DateTimeField(
+        label='Reservation Start',
+        widget=forms.TextInput(attrs={'type': 'datetime-local'})
+    )
+    reservation_end = forms.DateTimeField(
+        label='Reservation End',
+        widget=forms.TextInput(attrs={'type': 'datetime-local'})
+    )
+    table_category = forms.ModelChoiceField(
+        label='Table Category',
+        queryset=Table.objects.all(),
+        to_field_name='category'
+    )
