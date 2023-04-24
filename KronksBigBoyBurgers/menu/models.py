8500499396516aed920c443e4366160ae7ab6f9a
Dom123x456 <>
2023-04-24 16:05:00 +0000

    Added the CATEGORY_CHOICES to MenuItem model options for menu categories

--- KronksBigBoyBurgers/menu/models.py
+++ KronksBigBoyBurgers/menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = (
+        ('burger', 'Burger'),
+        ('side', 'Side'),
+        ('drink', 'Drink'),
+    
