04f86777449c1e8d5c2b0a1b165914274e23d6fa
Dom123x456 <>
2023-04-24 16:57:41 +0000

    adds admin interface for MenuItem with list display and filter options

--- KronksBigBoyBurgers/menu/admin.py
+++ KronksBigBoyBurgers/menu/admin.py
@@ -3,3 +3,7 @@ from .models import MenuItem
 
 # Register your models here.
 
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_filter = ('category', 'price')
+    list_display = ('name', 'category', 'price')
