1b29df9fa36d2b3bcf7a319b755529458ddc7d6d
Dom123x456 <>
2023-04-24 16:36:38 +0000

    Added URLs for menus and menu categories using class-based views

--- KronksBigBoyBurgers/menu/urls.py
+++ KronksBigBoyBurgers/menu/urls.py
@@ -1,3 +1,8 @@
 from menu import views
 from django.urls import path
 
+urlpatterns = [
+    path('', views.menus, name='menus'),
+    path('food_menu', views.FoodMenu.as_view(), name='food_menu'),
+    path('drinks_menu', views.DrinksMenu.as_view(), name='drinks_menu'),
+]
