783c5fee84941762774a076304560a838149d135
Dom123x456 <>
2023-04-24 21:35:59 +0000

    Add menu view to render menu items to the menu template

--- KronksBigBoyBurgers/menu/views.py
+++ KronksBigBoyBurgers/menu/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 from .models import MenuItem
 # Create your views here.
+
+def menu(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'menu/menu.html', {'menu_items': menu_items})
