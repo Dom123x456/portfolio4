e12b4465520359f3657c04be1edbc284658dde4c
Dom123x456 <>
2023-04-24 16:12:59 +0000

    Add MenuItem model with name, description, price, category, image fields

--- KronksBigBoyBurgers/menu/models.py
+++ KronksBigBoyBurgers/menu/models.py
@@ -8,3 +8,20 @@ class MenuItem(models.Model):
         ('side', 'Side'),
         ('drink', 'Drink'),
     
+    from django.db import models
+
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = (
+        ('burger', 'Burger'),
+        ('side', 'Side'),
+        ('drink', 'Drink'),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='menu_items/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
