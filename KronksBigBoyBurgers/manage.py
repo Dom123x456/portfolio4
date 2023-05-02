5eed21a773ed58be43614658a1c1f448afd9b3d9
Dom123x456 <>
2023-05-02 23:27:33 +0000

    Import the os set the LD_LIBRARY_PATH environment variable to '/usr/lib'

--- KronksBigBoyBurgers/manage.py
+++ KronksBigBoyBurgers/manage.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
+
 import sys
+import os
+os.environ['LD_LIBRARY_PATH'] = '/usr/lib'
+
+
 
 
 def main():
