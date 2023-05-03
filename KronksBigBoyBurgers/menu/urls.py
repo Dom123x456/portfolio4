57cd83d054a110c2c74e9c708d304cb72f9c78d8
Dom123x456 <>
2023-05-03 00:25:10 +0000

    Add URL patterns for editing menu items and fixed typo menus to menu

--- KronksBigBoyBurgers/menu/urls.py
+++ KronksBigBoyBurgers/menu/urls.py
@@ -1,10 +1,10 @@
 from menu import views
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('', views.menus, name='menus'),
+    path('', views.menus, name='menu'),
     path('food_menu', views.FoodMenu.as_view(), name='food_menu'),
     path('drinks_menu', views.DrinksMenu.as_view(), name='drinks_menu'),
-    path('menu/', include('menu.urls')),
+    path('menu/edit/', views.edit_menu_item, name='edit_menu_item'),
+    path('menu/edit/<int:item_id>/', views.edit_menu_item, name='edit_menu_item_edit'),
 ]
