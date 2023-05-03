9845b604c1733c21f60b8aefbdbf369022ba6b29
Dom123x456 <>
2023-05-03 00:29:00 +0000

    Add register and login view functions

--- KronksBigBoyBurgers/restaurant/forms.py
+++ KronksBigBoyBurgers/restaurant/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import TableCategory
 
+
 class AvailabilityForm(forms.Form):
     reservation_start = forms.DateTimeField(
         label='Reservation Start',
@@ -11,7 +12,30 @@ class AvailabilityForm(forms.Form):
         widget=forms.TextInput(attrs={'type': 'datetime-local'})
     )
     table_category = forms.ModelChoiceField(
-    label='Table Category',
-    queryset=TableCategory.objects.all(),
+        label='Table Category',
+        queryset=TableCategory.objects.all(),
     )
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    return render(request, 'login.html')
+
+
 
