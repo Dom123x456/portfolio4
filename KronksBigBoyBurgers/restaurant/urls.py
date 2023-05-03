1bf4638fc1926bad913d844c4520aa0d8d512fbf
Dom123x456 <>
2023-05-03 11:22:04 +0000

    Added URL patterns for register, login, user dashboard, admin dashboard

--- KronksBigBoyBurgers/restaurant/urls.py
+++ KronksBigBoyBurgers/restaurant/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path("reservation-list/", ReservationListView.as_view(), name="ReservationListView"),
     path("table-detail/<str:category>/", TableDetailView.as_view(), name="TableDetailView"),
     path("cancel-reservation/<int:pk>/", CancelReservationView.as_view(), name="CancelReservationView"),
-    path("checkout/", CheckoutView.as_view(), name="CheckoutView"),  
+    path("checkout/", CheckoutView.as_view(), name="CheckoutView"),
+    path("register/", register, name="register"),  
+    path("login/", login_view, name="login"),  
+    path("user-dashboard/", user_view, name="user_dashboard"),  
+    path("admin-dashboard/", admin_view, name="admin_dashboard"),  
 ]
